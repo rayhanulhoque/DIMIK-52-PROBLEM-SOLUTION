15f311cd1b70ed6f71da968a1b5e8b2a3cad46f4
RAYHANUL HOQUE JOY <126192508+rayhanulhoque@users.noreply.github.com>
2024-01-01 12:31:51 +0600

    Add files via upload

---
+++ 7 Count Numbers.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,s;
+    cin>>t;
+    for (int i=0;i<t;i++){
+
+
+        int array[s];
+        cin>>array[s];
+
+
+
+
+
+    }
+
+     return 0;
+}
