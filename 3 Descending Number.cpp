49fe1346299afe2663ddac71a163470daa5fc1e2
RAYHANUL HOQUE JOY <126192508+rayhanulhoque@users.noreply.github.com>
2024-01-01 12:29:32 +0600

    Add files via upload

---
+++ 3 Descending Number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int n = 1000;
+    for(int i =0;i<200;i++){
+
+        for(int i = 0;i<5;i++){
+            cout<<n--<<" ";
+        }
+       cout<<endl;
+    }
+
+    return 0;
+
+}
