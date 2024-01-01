f1f561797229c26721798a97de44b666d403780a
RAYHANUL HOQUE JOY <126192508+rayhanulhoque@users.noreply.github.com>
2024-01-01 12:30:32 +0600

    Add files via upload

---
+++ 5 Box 1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n,i,j,m,k;
+
+    cin>>t;
+    for(k=0;k<t;k++){
+            cin>>m;
+    for(i=1;i<=m;i++){
+        for(j=1;j<=m;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    if(k<t-1){
+    cout<<endl;
+    }
+  }
+    return 0;
+}
